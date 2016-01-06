60eee1a64ae38fce2666afc3e886249d6ac7c55e
Igor Ribeiro <U06IXR@macys.com>
2016-01-06 02:03:05 -0200

    Add more HTTP methods

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,36 +1,93 @@
 package com.mycompany.app;
 
 import static spark.Spark.*;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import com.mongodb.util.JSON;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
         MongoClientURI uri = new MongoClientURI("mongodb://example:example@candidate.54.mongolayer.com:10775,candidate.57.mongolayer.com:10128/Todo?replicaSet=set-5647f7c9cd9e2855e00007fb");
         MongoClient mongoClient = new MongoClient(uri);
-        DB db = mongoClient.getDB( "Todo" );
-        DBCollection collection = db.getCollection("todos");
+        MongoDatabase db = mongoClient.getDatabase("Todo");
+        MongoCollection<Document> collection = db.getCollection("todos");
 
-        get("/hello", (req, res) -> {
+        String ENV_PORT = System.getenv().get("PORT");
+        port( ENV_PORT == null ? 4567 : Integer.parseInt(ENV_PORT) );
+
+        get("/todos", (request, response) -> {
             List<DBObject> todos = new ArrayList<DBObject>();
-            try (DBCursor cursor = collection.find()) {
-                while(cursor.hasNext()) {
-                  todos.add(cursor.next());
+            FindIterable<Document> iterable = collection.find();
+            iterable.forEach(new Block<Document>() {
+                @Override
+                public void apply(final Document document) {
+                    todos.add((DBObject) JSON.parse(document.toJson()));
                 }
-            }
+            });
             return todos;
         });
 
-        get("/hi", (req, res) -> {
-            DBObject myDoc = collection.findOne();
-            return myDoc;
-        })
+        get("/todos/:id", (request, response) -> {
+            String id = request.params(":id");
+            FindIterable<Document> iterable = collection.find(new Document("_id", new ObjectId(id)));
+            Document todo = iterable.first();
+            if (todo == null) {
+                response.status(404);
+                return "Resource not found";
+            } else {
+                return JSON.parse(todo.toJson());
+            }
+        });
+
+        post("/todos", (request, response) -> {
+            DBObject body = (DBObject) JSON.parse(request.body());
+            Object title = body.get("title");
+            Object completed = body.get("completed");
+            Document newTodo = new Document()
+                .append("title", title)
+                .append("completed", completed == null ? false : completed);
+
+            collection.insertOne(newTodo);
+            return newTodo;
+        });
+
+        put("/todos/:id", (request, response) -> {
+            String id = request.params(":id");
+            DBObject body = (DBObject) JSON.parse(request.body());
+            Object title = body.get("title");
+            Object completed = body.get("completed");
+            Document update = new Document();
+
+            if (title != null) {
+                update.append("title", title);
+            }
+
+            if (completed != null) {
+                update.append("completed", completed);
+            }
+
+            return collection.updateOne(
+                new Document("_id", new ObjectId(id)),
+                new Document("$set", update)
+            );
+        });
+
+        delete("/todos/:id", (request, response) -> {
+            String id = request.params(":id");
+            return collection.deleteOne(new Document("_id", new ObjectId(id)));
+        });
     }
 }
