c52c96f885caf9f51d08ea45c1d7cb0fbc1db3e4
Igor Ribeiro <U06IXR@macys.com>
2016-01-07 01:22:29 -0200

    Change URI

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -20,7 +20,7 @@ import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
-        MongoClientURI uri = new MongoClientURI("mongodb://example:example@candidate.54.mongolayer.com:10775,candidate.57.mongolayer.com:10128/Todo?replicaSet=set-5647f7c9cd9e2855e00007fb");
+        MongoClientURI uri = new MongoClientURI("mongodb://example:example@candidate.54.mongolayer.com:10775,candidate.57.mongolayer.com:10128/spark-server-with-mongo?replicaSet=set-5647f7c9cd9e2855e00007");
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase db = mongoClient.getDatabase("Todo");
         MongoCollection<Document> collection = db.getCollection("todos");
